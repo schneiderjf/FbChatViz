324f088736c6aa87d2d17c2a5bcbcb938bd3df59
Jan-Felix Schneider <jan-felix.schneider@gmx.de>
2018-03-21 21:31:27 -0400

    update html parser script

--- html_to_csv.py
+++ html_to_csv.py
@@ -1,11 +1,12 @@
-#### This script parses the HTML files in the messages folder into one csv file ####
-print("The html files are parsed...")
-print("This may take a few seconds.")
-
 import numpy as np
 import pandas as pd
 import re
 import os, os.path
+
+##### GENERATE A DATA FRAME FROM THE HTML FILES #####
+
+print('The HTML files are being parsed...')
+print('This may take a few seconds. ')
 
 i = 0
 # delete /n
@@ -47,5 +48,60 @@ for i in range(no_files):
     conversation['plattform'] = 'Facebook'
     fb_database = pd.concat([fb_database,conversation])
 
-# Export to CSV
+##### PARSE THE HTML FILE #####
+
+def get_user(x):
+    matchobj = re.search(r'<span class="user">(.*?)</span>',x)
+    if matchobj:
+        y = matchobj.group(1)
+    else: 
+        y = None
+    return y
+
+def get_text(x):
+    matchobj = re.search(r'</div></div><p>(.*?)</p>',x)
+    if matchobj:
+        y = matchobj.group(1)
+    else: 
+        y = None
+    return y
+
+def get_day(x):
+    matchobj = re.search(r'class="meta">(.*?),',x)
+    if matchobj:
+        y = matchobj.group(1)
+    else: 
+        y = None
+    return y
+
+def get_time(x):
+    matchobj = re.search(r'at(.*?)</span>',x)
+    if matchobj:
+        y = matchobj.group(1)
+    else: 
+        y = None
+    return y
+
+def get_date(x):
+    matchobj = re.search(r'class="meta">.*?, (.*?) at',x)
+    if matchobj:
+        y = matchobj.group(1)
+    else: 
+        y = None
+    return y
+
+# parse the features
+
+fb_database['user'] = fb_database[0].apply(lambda x: get_user(x))
+fb_database['text'] = fb_database[0].apply(lambda x: get_text(x))
+fb_database['day'] = fb_database[0].apply(lambda x: get_day(x))
+fb_database['date'] = fb_database[0].apply(lambda x: get_date(x))
+fb_database['time'] = fb_database[0].apply(lambda x: get_time(x))
+
+fb_database = fb_database.reset_index().drop([0,'index'], axis=1)
+fb_database.index.name = 'message_id'
+
+##### EXPORT THE RESULTS #####
+
 fb_database.to_csv('fb_data.csv')
+print('Done! -> File at fb_data.csv')
